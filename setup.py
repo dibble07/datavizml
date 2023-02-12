2c4dffd4239dea81091a4bcf19d22a56019df149
robertdibble <robertdibble@live.co.uk>
2023-02-12 17:05:01 +0000

    upload package

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readme_file:
+    readme = readme_file.read()
+
+requirements = ['matplotlib', 'numpy', 'pandas', 'ppscore', 'scikit-learn', 'statsmodels']
+
+setup(
+    name='DataVizML',
+    version='0.0.1',
+    author='Robert Dibble',
+    author_email='robertdibble@live.co.uk',
+    description='A package to explore and visualise a dataset in preparation for an ML project',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/dibble07/datavizml',
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+)
