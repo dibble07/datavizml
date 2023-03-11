changed to interdecile skew and fixed error in calculation

datavizml/utils.py
@@ -77,18 +77,18 @@ def class_rebalance(x, y):
     return x_balanced, y_balanced
 
 
-# inter-quartile skew
-def inter_quartile_skew(data):
-    """A function to calculate inter-quartile skew"""
+# inter-decile skew
+def inter_decile_skew(data):
+    """A function to calculate inter-decile skew"""
 
-    lower, median, upper = np.quantile(data.dropna(), [0.25, 0.5, 0.75])
+    lower, median, upper = np.quantile(data.dropna(), [0.1, 0.5, 0.9])
     middle = (upper + lower) / 2
     range_ = abs(upper - lower)
     if range_ != 0:
-        feature_score = abs((median - middle)) / range_ / 2
+        feature_score = abs((median - middle)) / (range_ / 2)
     else:
         feature_score = 0
-    feature_score_type = "Inter-quartile skew"
+    feature_score_type = "Inter-decile skew"
 
     return feature_score, feature_score_type
 

tests/test_singledistribution.py
@@ -163,10 +163,10 @@ def test_combinations(dtype_feature, dtype_target, target_rebalance, feature_des
     expected_feature_transform = None
     if dtype_feature in ["Int64", "Float64"]:
         if feature_deskew:
-            expected_feature_score = 0.036
+            expected_feature_score = 0.151
             expected_feature_transform = "yeojohnson"
         else:
-            expected_feature_score = 0.139
+            expected_feature_score = 0.750
     elif dtype_feature in ["string", "category"]:
         expected_feature_score = 0.5
     elif dtype_feature in ["boolean"]:
@@ -214,7 +214,7 @@ def test_combinations(dtype_feature, dtype_target, target_rebalance, feature_des
     assert summary["feature_dtype"] == feature_str
     assert np.round(summary["feature_score"], 3) == expected_feature_score
     assert (
-        summary["feature_score_type"] == "Inter-quartile skew"
+        summary["feature_score_type"] == "Inter-decile skew"
         if dtype_feature in ["Int64", "Float64"]
         else "Categorical skew"
     )