b5cefa3282aa0f094e9694cf097f9d687c355a2e
robertdibble <robertdibble@live.co.uk>
2023-04-07 16:33:17 +0100

    Revert "test failing isort and mypy"
    This reverts commit ac1aae172885fe4eee39da573a9776b61c23b0ce.

--- datavizml/exploratorydataanalysis.py
+++ datavizml/exploratorydataanalysis.py
@@ -1,12 +1,14 @@
+from typing import Any, Dict, Optional, Union
+
 import matplotlib
 import numpy as np
-from datavizml import singledistribution as sd
-from datavizml import utils
-from typing import Any, Dict, Optional, Union
 import pandas as pd
-from matplotlib import pyplot as plt
 import ppscore as pps
 import seaborn as sns
+from matplotlib import pyplot as plt
+
+from datavizml import singledistribution as sd
+from datavizml import utils
 
 
 class ExploratoryDataAnalysis:
@@ -131,7 +133,7 @@ class ExploratoryDataAnalysis:
         :rtype: matplotlib.figure.Figure
         """
         # call the plot for each object
-        for plot in self:
+        for plot in self:  # type: ignore
             plot()
 
         return self.fig
