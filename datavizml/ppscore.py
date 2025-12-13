c6b022c3b6152446939ec61ca8486952122b39d1
Robert Dibble <robertdibble@live.co.uk>
2025-12-13 20:59:54 +0000

    simplify dtype checkers and normalizers

--- datavizml/ppscore.py
+++ datavizml/ppscore.py
@@ -1,15 +1,14 @@
-from sklearn import logger, tree
+from sklearn import tree
 from sklearn import preprocessing
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_absolute_error, f1_score
 
-import numpy as np
 import pandas as pd
 from pandas.api.types import (
     is_numeric_dtype,
     is_bool_dtype,
     is_object_dtype,
-    is_categorical_dtype as _is_categorical_dtype,
+    is_categorical_dtype,
     is_string_dtype,
     is_datetime64_any_dtype,
 )
@@ -17,50 +16,40 @@ from pandas.api.types import (
 random_seed = 123
 
 
-def is_categorical_dtype(series) -> bool:
-    "Determines if the dtype of the series represents categorical values"
+def _is_categorical(series) -> bool:
+    "Determines if series contains categorical values"
     return (
         is_bool_dtype(series)
         or is_object_dtype(series)
         or is_string_dtype(series)
-        or _is_categorical_dtype(series)
+        or is_categorical_dtype(series)
     )
 
 
-def _normalized_mae_score(model_mae, naive_mae):
-    "Normalizes the model MAE score, given the baseline score"
-    out = 0 if model_mae > naive_mae else 1 - (model_mae / naive_mae)
-    return out
+def _is_numeric(series) -> bool:
+    "Determines if series contains numeric values"
+    return is_numeric_dtype(series) and not is_bool_dtype(series)
 
 
 def _mae_normalizer(df, y, model_score):
-    "In case of MAE, calculates the baseline score for y and derives the PPS"
+    "Calculates the baseline score for y using MAE and derives the PPS"
     df["naive"] = df[y].median()
-    baseline_score = mean_absolute_error(df[y], df["naive"])
-    ppscore = _normalized_mae_score(abs(model_score), baseline_score)
-    return ppscore, baseline_score
-
-
-def _normalized_f1_score(model_f1, baseline_f1):
-    "Normalizes the model F1 score, given the baseline score"
-    out = 0 if model_f1 < baseline_f1 else (model_f1 - baseline_f1) / (1 - baseline_f1)
-    return out
+    baseline = mean_absolute_error(df[y], df["naive"])
+    ppscore = 0 if abs(model_score) > baseline else 1 - (abs(model_score) / baseline)
+    return ppscore, baseline
 
 
 def _f1_normalizer(df, y, model_score):
-    "In case of F1, calculates the baseline score for y and derives the PPS"
+    "Calculates the baseline score for y using F1 score and derives the PPS"
     df["truth"] = preprocessing.LabelEncoder().fit_transform(df[y])
     df["most_common_value"] = df["truth"].value_counts().index[0]
-    baseline_score = max(
+    truth_shuffled = df["truth"].sample(frac=1, random_state=random_seed)
+    baseline = max(
         f1_score(df["truth"], df["most_common_value"], average="weighted"),
-        f1_score(
-            df["truth"],
-            df["truth"].sample(frac=1, random_state=random_seed),
-            average="weighted",
-        ),
+        f1_score(df["truth"], truth_shuffled, average="weighted"),
     )
-    ppscore = _normalized_f1_score(model_score, baseline_score)
-    return ppscore, baseline_score
+    ppscore = 0 if model_score < baseline else (model_score - baseline) / (1 - baseline)
+    return ppscore, baseline
 
 
 VALID_CALCULATIONS = {
@@ -86,8 +75,8 @@ VALID_CALCULATIONS = {
         "model": tree.DecisionTreeClassifier(),
         "score_normalizer": _f1_normalizer,
     },
-    "predict_itself": {  # remove this task entirely if possible
-        "type": "predict_itself",
+    "predict_self": {
+        "type": "predict_self",
         "is_valid_score": True,
         "model_score": 1,
         "baseline_score": 0,
@@ -103,7 +92,7 @@ VALID_CALCULATIONS = {
 def _determine_case_and_prepare_df(df, x, y):
     "Returns str with the name of the determined case based on the columns x and y"
     if x == y:
-        return df, "predict_itself"
+        return df, "predict_self"
 
     df = df[[x, y]]
     df = df.dropna()
@@ -113,9 +102,9 @@ def _determine_case_and_prepare_df(df, x, y):
 
     df = df.sample(n=min(10_000, len(df)), random_state=random_seed, replace=False)
 
-    if is_categorical_dtype(df[y]):
+    if _is_categorical(df[y]):
         return df, "classification"
-    if is_numeric_dtype(df[y]) and not is_bool_dtype(df[y]):
+    if _is_numeric(df[y]):
         return df, "regression"
     else:
         raise TypeError(
@@ -133,7 +122,7 @@ def _calculate_model_cv_score(df, target, feature, task):
 
     # preprocess feature
     array = df[feature].values.reshape(-1, 1)
-    if is_categorical_dtype(df[feature]):
+    if _is_categorical(df[feature]):
         feature_input = preprocessing.OneHotEncoder().fit_transform(array)
     else:
         feature_input = array
