ffac47402717e22fe39bfff8da0895ae090eb2cd
Robert Dibble <robertdibble@live.co.uk>
2025-12-14 15:47:37 +0000

    simplify imports from package

---
+++ datavizml/__init__.py
@@ -0,0 +1,7 @@
+from .exploratorydataanalysis import ExploratoryDataAnalysis
+from .singledistribution import SingleDistribution
+
+__all__ = [
+    "ExploratoryDataAnalysis",
+    "SingleDistribution",
+]
